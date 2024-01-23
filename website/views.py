eb9a17bb6585a2b2eb24fc2833e369dbcb5bff3d
muhammadfayyazkaleem <muhammadfayyazkaleem@gmail.com>
2024-01-23 15:34:33 +0500

    add some js and make home page functional

--- website/views.py
+++ website/views.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash , jsonify
 from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
@@ -10,4 +13,21 @@ def home():
         note = request.form.get('note')
         if len(note) < 1:
             flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id = current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!' , category='success')
     return render_template('home.html' , user = current_user)
+
+@views.route('/delete-note', methods = ['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get('noteId')
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    return jsonify({})
+
